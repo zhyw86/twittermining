1cce4977a139270164f308884c44742495b26495
Jiakuan Li <jiak94@uga.edu>
2015-11-11 15:28:28 -0500

    modify output

--- adaBoost.py
+++ adaBoost.py
@@ -5,11 +5,14 @@ train_data = genfromtxt("./data.csv", delimiter=',')
 
 boost = AdaBoostClassifier()
 
-boost = boost.fit(train_data[0:100], train_data[0:100, 59])
+boost = boost.fit(train_data[0:10000], train_data[0:10000, 59])
 
 output = boost.predict(train_data)
 
-i = 0
+
 for x in output:
-    print i, ":", x
-    i = i + 1
+    status = 'unpopular'
+    if x > 1400:
+        status = 'popular'
+
+    print "%lf,%s" % (x, status)
