d4137d51722a7640639b6d69b8700c5bd2ecf9b1
ZW <zhyw86@gmail.com>
2015-11-13 12:26:37 -0500

    ...

--- randomforest.py
+++ randomforest.py
@@ -5,15 +5,16 @@ train_data = genfromtxt("./data.csv", delimiter=',')
 
 print train_data
 
-forest = RandomForestClassifier(n_estimators=400)
+forest = RandomForestClassifier(n_estimators=50)
 
 forest = forest.fit(train_data[0:10000], train_data[0:10000, 59])
 
 output = forest.predict(train_data)
 
-i = 1
+
 for x in output:
-    print i ,":", x, "\n"
-    i = i + 1
+    status = 'unpopular'
+    if x > 1400:
+        status = 'popular'
 
-
+    print "%lf,%s" % (x, status)
